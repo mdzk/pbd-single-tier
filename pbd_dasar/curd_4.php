0526182b27cb4da9e826a01441316e70713a1939
Muhammad Dzaky <mdzaky07@gmail.com>
2022-09-16 10:41:01 +0700

    aan

--- pbd_dasar/curd_4.php
+++ pbd_dasar/curd_4.php
@@ -35,3 +35,32 @@ switch ($a) {
 }
 mysqli_close($hub);
 ?>
+
+<!-- Baris terakhir 162 -->
+<?php
+function create_prodi() {
+    global $hub;
+    global $_POST;
+    $query = "INSERT INTO dt_prodi (kdprodi, nmprodi, akreditasi) VALUES";
+    $query .= " ('" .$_POST["kdprodi"]."', '" .$_POST["nmprodi"]."',
+    '".$_POST["akreditasi"]."')";
+    mysqli_query($hub, $query) or die(mysql_error());
+}
+
+function update_prodi(){
+    global $hub;
+    global $_POST;
+    $query = "UPDATE dt_prodi";
+    $query = " SET kdprodi='" .$_POST["kdprodi"]."', nmprodi= '".
+    $_POST ["nmprodi"]."', akreditasi='".$_POST["akreditasi"]."'";
+    $query .= " WHERE idprodi = ".$_POST["idprodi"];
+    mysqli_query($hub, $query) or die (mysql_error());
+}
+
+function delete_prodi() {
+    global $hub;
+    global $_POST;
+    $query .= " WHERE idprodi = ".$_POST["idprodi"];
+    mysqli_query($hub, $query) or die (mysql_error());
+}
+?>
